1ca929ac333c147bcee2012999e73d2ef0c6e738
Study <study@Romans-MacBook-Pro.local>
2019-03-20 23:46:49 +0300

    Change block number

--- index.ts
+++ index.ts
@@ -74,7 +74,7 @@ async function start() {
 
     const actionReader = new MongoActionReader({
         mongoEndpoint: mongoEndpoint,
-        startAtBlock: 19369100, // start at block number, must be no less than the value in the _index_state
+        startAtBlock: 19379660, // start at block number, must be no less than the value in the _index_state
         onlyIrreversible: false,
         dbName: mongoName
     })
